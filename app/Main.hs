bcb31a75dac39c2e61680eb7a8db3e18a0748eb3
Shane Drury <shane.drury@freeagent.com>
2020-02-09 14:14:16 +0000

    Spit out csv file

--- app/Main.hs
+++ app/Main.hs
@@ -11,18 +11,24 @@ import Lib
 import Data.Time
 import GHC.Generics
 import Data.Aeson
-import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as S
 import Polysemy
 import Polysemy.Embed
 import Data.Either
+import qualified Data.Csv as CSV
+import System.FilePath
 
 data Transaction = Transaction {
-  url :: String,
-  amount :: String,
+  --url :: String,
   dated_on :: Day,
-  description :: String
+  description :: String,
+  amount :: String
   --full_description :: String
-                                 } deriving (Eq, Generic, Show, FromJSON)
+                                 } deriving (Eq, Generic, Show, FromJSON, CSV.ToRecord)
+
+instance CSV.ToField Day where
+  toField d = BS.pack $ showGregorian d
 
 data TransactionsEndpoint = TransactionsEndpoint {
   bank_transactions :: [Transaction]
@@ -37,12 +43,14 @@ runTransactionsProviderOnFile :: (Members '[Embed IO] r) => FilePath -> Sem (Tra
 runTransactionsProviderOnFile fp = interpret $ \case
   GetTransactions -> embed $ bank_transactions <$> fromRight undefined <$> eitherDecodeFileStrict fp
 
-runapp sem = runM . runTransactionsProviderOnFile "sample.json" $ sem
+runapp sem = runM . runTransactionsProviderOnFile
+  ("/Users" </> "shanedrury" </> "repos" </> "htransaction" </> "sample.json") $ sem
 
 app :: (Members '[Embed IO, TransactionsProvider] r) => Sem r ()
 app = do
   transactions <- getTransactions
-  embed $ print transactions
+  let csv = CSV.encode transactions
+  embed $ S.putStrLn csv
 
 main :: IO ()
 main = do
