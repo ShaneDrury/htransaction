5c0620cacb1a29c17520cb7d92d1dad5d3d1f7b0
Shane Drury <shane.drury@freeagent.com>
2021-03-05 15:45:23 +0000

    Clean up DB runners

--- src/Db.hs
+++ src/Db.hs
@@ -104,27 +104,26 @@ updateTokens_ tokenSet = upsert tokenSet []
 
 runDbMOnSqlite :: (Members '[Embed IO] r) => FilePath -> InterpreterFor DbM r
 runDbMOnSqlite fp = interpret $ \case
-  FindByUuid uuid -> embed $ do
-    r <- P.runSqlite (pack fp) (findByUuid_ uuid)
+  FindByUuid uuid -> runQ $ do
+    r <- findByUuid_ uuid
     return $ entityVal <$> r
-  InsertUnique tx -> embed $ P.runSqlite (pack fp) (P.insertUnique tx)
-  GetAccount ref -> embed $
-    P.runSqlite (pack fp) $ do
-      account <- accountByReference ref
-      return $ entityVal <$> account
-  UpdateLastImported bankAccountId lastImported -> embed $ P.runSqlite (pack fp) $ updateLastImported_ bankAccountId lastImported
-  GetTokensByInstitution institution -> embed $
-    P.runSqlite (pack fp) $ do
-      results <- getTokensByInstitution_ institution
-      return $ entityVal <$> results
-  UpsertTokens tokens -> embed $
-    P.runSqlite (pack fp) $ do
-      r <- updateTokens_ tokens
-      return $ entityVal r
-  GetClient institution -> embed $
-    P.runSqlite (pack fp) $ do
-      result <- selectFirst [ClientInstitution P.==. institution] []
-      return $ entityVal <$> result
+  InsertUnique tx -> runQ (P.insertUnique tx)
+  GetAccount ref -> runQ $ do
+    account <- accountByReference ref
+    return $ entityVal <$> account
+  UpdateLastImported bankAccountId lastImported -> runQ $ updateLastImported_ bankAccountId lastImported
+  GetTokensByInstitution institution -> runQ $ do
+    results <- getTokensByInstitution_ institution
+    return $ entityVal <$> results
+  UpsertTokens tokens -> runQ $ do
+    r <- updateTokens_ tokens
+    return $ entityVal r
+  GetClient institution -> runQ $ do
+    result <- selectFirst [ClientInstitution P.==. institution] []
+    return $ entityVal <$> result
+  where
+    runQ :: Members '[Embed IO] r => SqlPersistM a -> Sem r a
+    runQ qry = embed $ P.runSqlite (pack fp) qry
 
 runMigrations :: (Members '[Embed IO] r) => FilePath -> Sem r ()
 runMigrations fp = embed $ P.runSqlite (pack fp) (runMigration migrateAll)
